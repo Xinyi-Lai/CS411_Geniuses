528a6f397d489a4507b859eec818b2a6ddf6f849
ZhuKerui <kerui.17@intl.zju.edu.cn>
2020-07-15 03:16:18 +0800

    simple login without information check

---
+++ role_select_page.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <input type="button" onclick="window.location.href='search_page.php'" value="I want to buy ...">
+        <input type="button" onclick="window.location.href='my_sales_page.php'" value="I want to sell ...">
+    </body>
+</html>
