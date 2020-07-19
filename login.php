41e1437781826cdbc5f88efed906857798a1498d
baozi008 <hanyin.17@intl.zju.edu.cn>
2020-07-18 22:30:31 -0500

    hhhhh

--- login.php
+++ login.php
@@ -38,7 +38,7 @@
     <title>BEACON - Login</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="baguetteBox.min.css">
     <link rel="stylesheet" href="assets/css/smoothproducts.css">
 </head>
 
