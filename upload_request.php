d2d38820373f6a4a3d2d636cd724140b7c33ac88
GarboLou <loujiaqi1998@126.com>
2020-07-21 19:01:15 +0800

    request

--- upload_request.php
+++ upload_request.php
@@ -11,21 +11,35 @@
     // $SaleId = (double)ppc($_POST["SaleId"]);
 
     if ($product_name != "" && 
-        $intended_price > 0) {
-                
-            /* Get the database */
-            $conn = connectDB();
-            /* Set the decode */
-            $sql = "INSERT INTO Requests(BuyerID, ProductName, Tag, Description, IntendedPrice) 
-                    VALUES ('$buyer_id', '$product_name', '$tag', '$description', $intended_price)";
-            $success = $conn->query($sql);
-            if ($success){
-                echo 'Insert success!';
-            }else{
-                echo 'Insert fail'.$conn->error;
-            }
+        $intended_price > 0 &&
+        file_exists($_FILES['image']['tmp_name'])) {
+            /* Get the directory to store the image */
+            $upload_dir="images/".$buyer_id."/";
+            if(!is_dir($upload_dir))
+                mkdir($upload_dir);
+            /* Get the postfix of the image */
+            $postfix = substr($_FILES['image']['name'],strrpos($_FILES['image']['name'],'.'));
+            /* Set the file name */
+            $filename = $upload_dir.date("YmdHis").$postfix;
+            /* Store the image to the server */
+            if(move_uploaded_file($_FILES['image']['tmp_name'], $filename)){
+                /* Get the database */
+                $conn = connectDB();
+                /* Set the decode */
+                $sql = "INSERT INTO Requests(BuyerID, ProductName, Tag, Description, Image, IntendedPrice) 
+                    VALUES ('$buyer_id', '$product_name', '$tag', '$description', '$filename', $intended_price)";
+                $success = $conn->query($sql);
+                if ($success){
+                    header("location:myproducts.php");
+                    exit;
+                }else{
+                    echo 'Insert fail'.$conn->error;
+                }
 
-            $conn->close();
+                $conn->close();
+            }else{
+                echo 'Store fail';
+            }
 
     }else{
         echo 'Wrong information';
