2b209caa94d2c3e90499522e5cd6d23ada45ff11
ZhuKerui <kerui.17@intl.zju.edu.cn>
2020-07-25 01:56:30 +0800

    empty database

--- dump/Beacon.sql
+++ dump/Beacon.sql
@@ -2,10 +2,10 @@
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: database
--- Generation Time: Jul 23, 2020 at 07:39 AM
--- Server version: 8.0.20
--- PHP Version: 7.4.6
+-- 主机： database
+-- 生成日期： 2020-07-24 14:33:40
+-- 服务器版本： 8.0.21
+-- PHP 版本： 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,34 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Beacon`
+-- 数据库： `Beacon`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Group_Mem`
---
-
-CREATE TABLE `Group_Mem` (
-  `Name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `NetId` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `Group_Mem`
---
-
-INSERT INTO `Group_Mem` (`Name`, `NetId`) VALUES
-('Hanyin Shao', 'hanyins2'),
-('Jiaqi Lou', 'jiaqil6'),
-('Kerui Zhu', 'keruiz2'),
-('Xinyi Lai', 'xlai7');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Requests`
+-- 表的结构 `Requests`
 --
 
 CREATE TABLE `Requests` (
@@ -56,22 +35,14 @@ CREATE TABLE `Requests` (
   `Description` varchar(500) DEFAULT NULL,
   `Image` varchar(100) DEFAULT NULL,
   `IntendedPrice` double DEFAULT NULL,
-  `SaleId` int DEFAULT NULL
+  `SaleId` int DEFAULT NULL,
+  `DatePost` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `Requests`
---
-
-INSERT INTO `Requests` (`RequestId`, `BuyerId`, `ProductName`, `Tag`, `Description`, `Image`, `IntendedPrice`, `SaleId`) VALUES
-(2, 'cs411', 'House', 'Daily Necessity', 'Warm', '', 30, NULL),
-(3, 'cs411', 'Mantou', 'Food', 'Yummy', '', 2, 12),
-(4, 'genius', 'CS 411 Textbook', 'Textbook', 'CS411 Textbook', '', 25, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Sales`
+-- 表的结构 `Sales`
 --
 
 CREATE TABLE `Sales` (
@@ -84,160 +55,124 @@ CREATE TABLE `Sales` (
   `IntendedPrice` double DEFAULT NULL,
   `OriginalPrice` double DEFAULT NULL,
   `Depreciation` int DEFAULT NULL,
-  `IntendedBuyerId` varchar(50) DEFAULT NULL
+  `IntendedBuyerId` varchar(50) DEFAULT NULL,
+  `DatePost` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `Sales`
---
-
-INSERT INTO `Sales` (`SaleId`, `SellerId`, `ProductName`, `Tag`, `Description`, `Image`, `IntendedPrice`, `OriginalPrice`, `Depreciation`, `IntendedBuyerId`) VALUES
-(6, 'cs411', 'Baozi', 'Food', 'Delicious', 'images/cs411/20200722132752.png', 100, 200, 9, 'genius'),
-(8, 'cs411', 'Another Island', 'Daily Necessity', 'Beautiful', 'images/cs411/20200722132849.jpg', 888, 999, 9, NULL),
-(9, 'cs411', 'Fake Genius', 'Toy', 'Sleepy and stupid', 'images/cs411/20200722132913.png', 1, 10, 9, NULL),
-(12, 'genius', 'Another Mouse', 'Toy', 'Cute', 'images/genius/20200722143115.png', 10, 12, 9, 'keruiz2');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Transactions`
+-- 表的结构 `Transactions`
 --
 
 CREATE TABLE `Transactions` (
   `TransactionId` int NOT NULL,
-  `SellerId` varchar(50) NOT NULL,
-  `BuyerId` varchar(50) NOT NULL,
-  `ProductName` varchar(50) NOT NULL,
-  `Price` double NOT NULL,
-  `Tag` varchar(50) NOT NULL,
-  `Description` varchar(500) NOT NULL
+  `SellerId` varchar(50) DEFAULT NULL,
+  `BuyerId` varchar(50) DEFAULT NULL,
+  `ProductName` varchar(50) DEFAULT NULL,
+  `Price` double DEFAULT NULL,
+  `Tag` varchar(50) DEFAULT NULL,
+  `Description` varchar(500) DEFAULT NULL,
+  `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `Transactions`
---
-
-INSERT INTO `Transactions` (`TransactionId`, `SellerId`, `BuyerId`, `ProductName`, `Price`, `Tag`, `Description`) VALUES
-(2, 'cs411', 'genius', 'Island', 1000, 'Daily Necessity', 'Beautiful'),
-(3, 'genius', 'cs411', 'Baozi', 1, 'Food', 'Good'),
-(4, 'genius', 'cs411', 'Mouse', 0.1, 'Toy', 'Cute'),
-(5, 'cs411', 'genius', 'Genius', 150, 'Toy', 'Smart and sharp'),
-(6, 'keruiz2', 'cs411', 'house', 2000, 'house', 'best house');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Users`
+-- 表的结构 `Users`
 --
 
 CREATE TABLE `Users` (
-  `NetId` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `Password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `Campus` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `Name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `Email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `Major` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `Year` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `NetId` varchar(50) NOT NULL,
+  `Password` varchar(50) DEFAULT NULL,
+  `Name` varchar(50) DEFAULT NULL,
+  `Email` varchar(50) DEFAULT NULL,
+  `Campus` varchar(10) DEFAULT NULL,
+  `Major` varchar(10) DEFAULT NULL,
+  `Year` varchar(10) DEFAULT NULL,
+  `DateJoin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `Users`
---
-
-INSERT INTO `Users` (`NetId`, `Password`, `Campus`, `Name`, `Email`, `Major`, `Year`) VALUES
-('cs411', 'cs411', 'UIUC', 'Little Genius', 'cs411@geniuses.com', 'CS', 'Freshman'),
-('genius', 'genius', 'UIUC', 'Real Genius', 'genius@geniuses.com', 'CEE', 'graduate'),
-('hanyins2', 'hanyins2', 'UIUC', 'Hanyin Shao', 'hanyins2@illinois.edu', 'ME', 'Freshman'),
-('jiaqil6', 'jiaqil6', 'ZJUIntl', 'Jiaqi Lou', 'jiaqil6@illinois.edu', 'ECE', 'Junior'),
-('keruiz2', 'keruiz2', 'ZJUIntl', 'Kerui Zhu', 'keruiz2@illinois.edu', 'ECE', 'Junior'),
-('xlai7', 'xlai7', 'ZJUIntl', 'Xinyi Lai', 'xlai7@illinois.edu', 'EE', 'Senior');
-
---
--- Indexes for dumped tables
+-- 转储表的索引
 --
 
 --
--- Indexes for table `Group_Mem`
---
-ALTER TABLE `Group_Mem`
-  ADD PRIMARY KEY (`NetId`);
-
---
--- Indexes for table `Requests`
+-- 表的索引 `Requests`
 --
 ALTER TABLE `Requests`
   ADD PRIMARY KEY (`RequestId`),
-  ADD KEY `BuyerId` (`BuyerId`),
-  ADD KEY `SaleId` (`SaleId`);
+  ADD KEY `Requests_ibfk_1` (`BuyerId`),
+  ADD KEY `Requests_ibfk_2` (`SaleId`);
 
 --
--- Indexes for table `Sales`
+-- 表的索引 `Sales`
 --
 ALTER TABLE `Sales`
   ADD PRIMARY KEY (`SaleId`),
-  ADD KEY `SellerId` (`SellerId`),
-  ADD KEY `IntendedBuyerId` (`IntendedBuyerId`);
+  ADD KEY `Sales_ibfk_1` (`SellerId`),
+  ADD KEY `Sales_ibfk_2` (`IntendedBuyerId`);
 
 --
--- Indexes for table `Transactions`
+-- 表的索引 `Transactions`
 --
 ALTER TABLE `Transactions`
   ADD PRIMARY KEY (`TransactionId`),
-  ADD KEY `Transactions_ibfk_1` (`SellerId`),
-  ADD KEY `Transactions_ibfk_2` (`BuyerId`);
+  ADD KEY `Transactions_ibfk_1` (`BuyerId`),
+  ADD KEY `Transactions_ibfk_2` (`SellerId`);
 
 --
--- Indexes for table `Users`
+-- 表的索引 `Users`
 --
 ALTER TABLE `Users`
   ADD PRIMARY KEY (`NetId`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- 在导出的表使用AUTO_INCREMENT
 --
 
 --
--- AUTO_INCREMENT for table `Requests`
+-- 使用表AUTO_INCREMENT `Requests`
 --
 ALTER TABLE `Requests`
-  MODIFY `RequestId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `RequestId` int NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `Sales`
+-- 使用表AUTO_INCREMENT `Sales`
 --
 ALTER TABLE `Sales`
-  MODIFY `SaleId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `SaleId` int NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `Transactions`
+-- 使用表AUTO_INCREMENT `Transactions`
 --
 ALTER TABLE `Transactions`
-  MODIFY `TransactionId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `TransactionId` int NOT NULL AUTO_INCREMENT;
 
 --
--- Constraints for dumped tables
+-- 限制导出的表
 --
 
 --
--- Constraints for table `Requests`
+-- 限制表 `Requests`
 --
 ALTER TABLE `Requests`
-  ADD CONSTRAINT `Requests_ibfk_1` FOREIGN KEY (`BuyerId`) REFERENCES `Users` (`NetId`) ON DELETE CASCADE,
-  ADD CONSTRAINT `Requests_ibfk_2` FOREIGN KEY (`SaleId`) REFERENCES `Sales` (`SaleId`) ON DELETE SET NULL;
+  ADD CONSTRAINT `Requests_ibfk_1` FOREIGN KEY (`BuyerId`) REFERENCES `Users` (`NetId`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `Requests_ibfk_2` FOREIGN KEY (`SaleId`) REFERENCES `Sales` (`SaleId`) ON DELETE CASCADE ON UPDATE RESTRICT;
 
 --
--- Constraints for table `Sales`
+-- 限制表 `Sales`
 --
 ALTER TABLE `Sales`
-  ADD CONSTRAINT `Sales_ibfk_1` FOREIGN KEY (`SellerId`) REFERENCES `Users` (`NetId`) ON DELETE CASCADE,
-  ADD CONSTRAINT `Sales_ibfk_2` FOREIGN KEY (`IntendedBuyerId`) REFERENCES `Users` (`NetId`) ON DELETE SET NULL;
+  ADD CONSTRAINT `Sales_ibfk_1` FOREIGN KEY (`SellerId`) REFERENCES `Users` (`NetId`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `Sales_ibfk_2` FOREIGN KEY (`IntendedBuyerId`) REFERENCES `Users` (`NetId`) ON DELETE CASCADE ON UPDATE RESTRICT;
 
 --
--- Constraints for table `Transactions`
+-- 限制表 `Transactions`
 --
 ALTER TABLE `Transactions`
-  ADD CONSTRAINT `Transactions_ibfk_1` FOREIGN KEY (`SellerId`) REFERENCES `Users` (`NetId`) ON DELETE CASCADE ON UPDATE RESTRICT,
-  ADD CONSTRAINT `Transactions_ibfk_2` FOREIGN KEY (`BuyerId`) REFERENCES `Users` (`NetId`) ON DELETE CASCADE ON UPDATE RESTRICT;
+  ADD CONSTRAINT `Transactions_ibfk_1` FOREIGN KEY (`BuyerId`) REFERENCES `Users` (`NetId`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `Transactions_ibfk_2` FOREIGN KEY (`SellerId`) REFERENCES `Users` (`NetId`) ON DELETE CASCADE ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
